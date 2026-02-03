0ed4138233334270a0ceb98c0157ef70f9a7a17d
CcBoom <1039776732@qq.com>
2026-02-03 15:28:04 +0800

    SPM

---
+++ Test/test.swift
@@ -0,0 +1,23 @@
+//
+//  est.swift
+//  ZZStoreKit
+//
+//  Created by 陈钟 on 2026/2/3.
+//
+import Testing
+@testable import ZZStoreKit
+
+@Test func test() async throws {
+//    ZZStoreKit.complateTransaction { transactions in
+//
+//    }
+//
+//    ZZStoreKit.getProducts([]) { result in
+//        switch result {
+//            case .success(let data):
+//                break
+//            case .failed(let err):
+//                break
+//        }
+//    }
+}
